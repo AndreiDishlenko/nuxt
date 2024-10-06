fde61c25ae73a410c1ebb503e9d37fbd16e430e6
Andrei Dishlenko <a.dishlenko@gmail.com>
2024-10-06 16:01:01 +0300

    v.1.0.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+    // runtimeConfig: {
+    //     NITRO_UNIX_SOCKET: process.env.NITRO_UNIX_SOCKET
+    // },
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    nitro: {
+        socket: '/var/www/che6db9f89/.system/nodejs/cityposio.com.ua/socket'
+    }
 })
