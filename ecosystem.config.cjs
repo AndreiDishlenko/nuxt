59e6e8a7155f65a41f2b90642f21559e8e39f289
Andrei Dishlenko <a.dishlenko@gmail.com>
2024-10-10 18:36:34 +0300

    v.1.0.2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,17 @@
-// app.config.ts
-export default defineAppConfig({
-    icon: {
-      size: '24px', // default <Icon> size applied
-      class: 'icon', // default <Icon> class applied
-      mode: 'css', // default <Icon> mode applied
-      aliases: {
-        'myicon': '/icons/telegram.svg',
-      }
-    }
-})  
+module.exports = {
+    apps: [
+        {
+            name: 'nuxtff.com',
+            port: '3000',
+            exec_mode: 'cluster',
+            instances: 'max',
+            script: '.output/server/index.mjs',
+            env: {
+                NODE_ENV: 'production',
+                NITRO_PORT: 3000,
+                NITRO_HOST: localhost
+            },
+        }
+    ]
+}
+  
