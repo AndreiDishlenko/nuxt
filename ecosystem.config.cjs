d69f3848b22ea43932f9a8d8f3de2b4f938f4a59
Andrei Dishlenko <a.dishlenko@gmail.com>
2024-10-10 20:52:05 +0300

    v.1.0.2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
                 NODE_ENV: 'production',
                 NITRO_PORT: 3000,
                 NITRO_HOST: '127.0.0.1',
-                NODE_OPTIONS: '--max-old-space-size=4096 --disable-warning=DEP0155 --disable-warning=DEP0166'
+                // NODE_OPTIONS: '--max-old-space-size=4096 --disable-warning=DEP0155 --disable-warning=DEP0166'
             },
         }
     ]
